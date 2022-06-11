f8dc5d147a5360340d2429de0d255b05a46555f7
ZachhS <84425138+ZachhS@users.noreply.github.com>
2022-06-11 13:13:15 -0400

    New Site

---
+++ port.js
@@ -0,0 +1,26 @@
+
+// Portfolio JS
+
+const burgerIcon = document.querySelector('.burgerIcon');
+const mobileMenu = document.querySelector('.mobileMenu');
+const mobileMenuOverlay = document.querySelector('.menuOverlay');
+
+const openMobile = () => {
+    mobileMenu.classList.add('active');
+    mobileMenuOverlay.classList.add('active');
+}
+
+const closeMobile = () => {
+    mobileMenu.classList.remove('active');
+    mobileMenuOverlay.classList.remove('active');
+}
+
+const manipulateWave = (e) => {
+    console.log(theTitle);
+    theTitle.classList.add('move');
+}
+
+burgerIcon.addEventListener('click', openMobile);
+mobileMenuOverlay.addEventListener('click', closeMobile);
+
+
